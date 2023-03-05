5b03915698584c19429e2139ef9a566c84ba4e88
Dawon00 <da6921@naver.com>
2023-03-05 19:04:08 +0900

    feat: 모집대상, 모집일정, FAQ Component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import './App.css';
-import Achievement from './components/Achievement';
-import MainaAtivity from './components/MainActivity';
-import TabCommunity from './components/TabCommunity';
-import TabRecruit from './components/TabRecruit';
+import "./App.css";
+import Achievement from "./components/Achievement";
+import MainaAtivity from "./components/MainActivity";
+import TabCommunity from "./components/TabCommunity";
+import TabRecruit from "./components/TabRecruit";
 import TabPhoto from "./components/TabPhoto";
 import ClubRoom from "./components/ClubRoom";
-import WithTab from './components/WithTab';
-
+import WithTab from "./components/WithTab";
+import Target from "./components/Target";
+import Schedule from "./components/Schedule";
+import FAQ from "./components/FAQ";
 
 function App() {
   return (
     <div>
-
-      <div className='bg-[#865DFF] flex flex-col items-center justify-center py-3'>
-        <img src='TAB_logo_textWhite.png' className='h-52'/>
-        <div className='bg-[#E384FF] rounded-xl p-2 px-3'>
-          <span className='text-white font-extrabold  '>
+      <div className="bg-[#865DFF] flex flex-col items-center justify-center py-3">
+        <img src="TAB_logo_textWhite.png" className="h-52" />
+        <div className="bg-[#E384FF] rounded-xl p-2 px-3">
+          <span className="text-white font-extrabold  ">
             Recruitment of new members for the 41st TAB
           </span>
-          </div>
+        </div>
       </div>
 
       <div className="mx-auto  w-full lg:w-[1024px] p-7 space-y-20 ">
-        <TabRecruit/>
-        <WithTab/>
-        <TabCommunity/>
-        <MainaAtivity/>
-        <Achievement/>
+        <TabRecruit />
+        <WithTab />
+        <TabCommunity />
+        <MainaAtivity />
+        <Achievement />
         <TabPhoto />
         <ClubRoom />
-      </div>  
-
+        <Target />
+        <Schedule />
+        <FAQ />
+      </div>
     </div>
   );
 }
--- src/components/TabPhoto.js
+++ src/components/TabPhoto.js
@@ -21,16 +21,16 @@ function TabPhoto() {
         onSwiper={(swiper) => console.log(swiper)}
       >
         <SwiperSlide>
-          <img src="엠티.jpg"></img>
+          <img className="rounded-3xl" src="엠티.jpg"></img>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="엠티.jpg"></img>
+          <img className="rounded-3xl" src="엠티.jpg"></img>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="엠티.jpg"></img>
+          <img className="rounded-3xl" src="엠티.jpg"></img>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="엠티.jpg"></img>
+          <img className="rounded-3xl" src="엠티.jpg"></img>
         </SwiperSlide>
       </Swiper>
       <div className="grid bg-[#F3F3F3] rounded-3xl	 p-5 mt-5">
