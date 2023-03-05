7852a6aa004773b7db8e30162f4e7156cb2313d8
juhui88 <sand8594@hufs.ac.kr>
2023-03-05 18:27:15 +0900

    Fix rounded

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
       <div className='bg-[#865DFF] flex flex-col items-center justify-center py-3'>
         <img src='TAB_logo_textWhite.png' className='h-52'/>
-        <div className='bg-[#E384FF] rounded-xl p-2 px-3'>
+        <div className='bg-[#E384FF] rounded-3xl p-2 px-3'>
           <span className='text-white font-extrabold  '>
             Recruitment of new members for the 41st TAB
           </span>
