3c0e3aa81449c0d1a633340c81eed8635f2fbb53
juhui88 <sand8594@hufs.ac.kr>
2023-08-31 15:36:25 +0900

    Fix 2023멤버 추가 및 세미

--- src/route/Program.js
+++ src/route/Program.js
@@ -1,13 +1,21 @@
 import { Link, Route, Routes } from "react-router-dom";
-import data from "../api/2022TAB_semina.json";
+import data_2022 from "../api/2022TAB_semina.json";
 import Bar from "../components/Bar";
 import Header from "../components/Header";
-import ProgramItem from "../components/program/ProgramItem"
+import ProgramItem from "../components/program/ProgramItem";
+import data_2023 from "../api/2023TAB_semina.json";
+import { useState } from "react";
 
 export default function Program() {
+  const [year, setYear] = useState(2023);
+  const data = year === 2023 ? data_2023 : data_2022;
+  const onClick = (i) => {
+    setYear(i);
+  };
+
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="mx-auto  w-full lg:w-[1024px] p-7">
         <span className="text-2xl tsm:text-3xl font-extrabold">
           지난 TAB의 활동들, 함께 보실까요?
@@ -18,9 +26,19 @@ export default function Program() {
           TAB에서 선후배/동기들과 함께 공부하고 성장해보세요. <br />
           다양한 스터디, 세미나가 준비되어있습니다.
         </div>
+        <div className="flex  w-24">
+          {[2022, 2023].map((i) => (
+            <div
+              onClick={() => onClick(i)}
+              className="cursor-pointer rounded-full text-lg p-2  font-bold hover:text-xl transition-all pb-3"
+            >
+              <span className={i === year ? "text-[#865DFF]" : ""}>{i}</span>
+            </div>
+          ))}
+        </div>
         <div className="grid sm:grid-cols-2 gap-12 pb-40">
           {data.map((d, i) => (
-            <Link to={`${i}`}>
+            <Link to={year === 2023 ? `${i + 20}` : `${i}`}>
               <ProgramItem imgSrc={d.imgSrc[0]} title={d.title} />
             </Link>
           ))}
--- src/route/ProgramDetail.js
+++ src/route/ProgramDetail.js
@@ -1,68 +1,93 @@
 import Bar from "../components/Bar";
-import tw from "tailwind-styled-components"
+import tw from "tailwind-styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import { Navigation } from "swiper";
-import json from "../api/2022TAB_semina.json"
-import { useParams } from "react-router-dom";
+import data_2022 from "../api/2022TAB_semina.json";
+import { Link, useParams } from "react-router-dom";
 import Header from "../components/Header";
+import data_2023 from "../api/2023TAB_semina.json";
 
 const Item = tw.div`
     flex
     flex-col
     space-y-3
-`
+`;
 const ItemTitle = tw.span`
     text-xl
     font-bold
-`
+`;
 const ItemDetail = tw.span`
     ml-5
-`
+`;
 export default function ProgramDetail() {
-    const {programId} = useParams();
-    const data = json[programId]
-    return(
-        <div>
-            <Header/>
-            <div className="mx-auto  w-full lg:w-[1024px] p-7 ">
-                <span className="text-2xl sm:text-3xl font-extrabold">{data.title}</span>
-                <img src ={data.imgSrc[0]} className="rounded-3xl w-full my-5"/>
-                <div className="bg-[#F3F3F3] p-5 rounded-3xl mb-5 text-xs sm:text-base">{data.intro}</div>
-                <div className=" space-y-10 text-xs sm:text-base">
-                    <Item className="flex flex-col">
-                        <ItemTitle>🚩 목표</ItemTitle>
-                        <ItemDetail >{data.goal}</ItemDetail>
-                    </Item>
-                    <Item>
-                        <ItemTitle>🗓️ 활동기간</ItemTitle>
-                        <ItemDetail>{data.period}</ItemDetail>
-                    </Item>
-                    <Item>
-                        <ItemTitle>👩‍💻 활동내용</ItemTitle>
-                        <div className="ml-5">
-                            {data.content.split("\n").map(i=><span>{i}<br/></span>)}
-                        </div>
-                    </Item>
-                    <Item>
-                        <ItemTitle>✨ 활동사진</ItemTitle>
-                        <Swiper
-                            className="mt-5 w-full h-96 bg-[#F3F3F3] rounded-3xl"
-                            modules={[Navigation]}
-                            spaceBetween={50}
-                            slidesPerView={1}
-                            navigation
-                            onSwiper={(swiper) => console.log(swiper)}
-                        >
-                       {data.imgSrc.map(i=><SwiperSlide><img src = {i} className="h-96 object-contain"/></SwiperSlide>)}
-                        </Swiper>
-                    </Item>
-                </div>
-            </div>
-            <Bar/>
+  const { programId } = useParams();
+  const data =
+    programId >= 20 ? data_2023[Number(programId) - 20] : data_2022[programId];
+  return (
+    <div>
+      <Header />
+      <div className="mx-auto  w-full lg:w-[1024px] p-7 ">
+        <span className="text-2xl sm:text-3xl font-extrabold ">
+          {data.title}
+        </span>
+        <img src={data.imgSrc[0]} className="rounded-3xl w-full my-5" />
+        <div className="bg-[#F3F3F3] p-5 rounded-3xl mb-5 text-xs sm:text-base">
+          {data.intro}
         </div>
-    )
+        <div className=" space-y-10 text-xs sm:text-base">
+          <Item className="flex flex-col">
+            <ItemTitle>🚩 목표</ItemTitle>
+            <ItemDetail>{data.goal}</ItemDetail>
+            {Number(programId) - 20 === 0 ? (
+              <div className="pl-5">
+                <Link
+                  className="text-blue-400 hover:font-bold"
+                  to="https://ian-react-project-emotiondiary.web.app/"
+                >
+                  감성 일기장 구경하러 가기
+                </Link>
+              </div>
+            ) : null}
+          </Item>
+          <Item>
+            <ItemTitle>🗓️ 활동기간</ItemTitle>
+            <ItemDetail>{data.period}</ItemDetail>
+          </Item>
+          <Item>
+            <ItemTitle>👩‍💻 활동내용</ItemTitle>
+            <div className="ml-5">
+              {data.content.split("\n").map((i) => (
+                <span>
+                  {i}
+                  <br />
+                </span>
+              ))}
+            </div>
+          </Item>
+          <Item>
+            <ItemTitle>✨ 활동사진</ItemTitle>
+            <Swiper
+              className="mt-5 w-full h-96 bg-[#F3F3F3] rounded-3xl"
+              modules={[Navigation]}
+              spaceBetween={50}
+              slidesPerView={1}
+              navigation
+              onSwiper={(swiper) => console.log(swiper)}
+            >
+              {data.imgSrc.map((i) => (
+                <SwiperSlide>
+                  <img src={i} className="h-96 object-contain" />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </Item>
+        </div>
+      </div>
+      <Bar />
+    </div>
+  );
 }
