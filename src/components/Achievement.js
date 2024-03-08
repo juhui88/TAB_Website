46a480dc844de8b67e01a677488d85160e7fbd1a
juhui88 <sand8594@naver.com>
2024-03-08 12:01:07 +0900

    Fix: Achievement add

--- src/components/Achievement.js
+++ src/components/Achievement.js
@@ -2,70 +2,126 @@ export default function Achievement() {
   return (
     <div>
       <span className="text-xl sm:text-2xl font-extrabold">이루어온 것들✨</span>
-      <div className="bg-[#F3F3F3] p-6 sm:p-10 sm:px-20 mt-10 rounded-3xl">
-        <div className="collapse bg-base-200">
-          <input type="checkbox" />
-          <div className="collapse-title text-xl font-medium">
-            2021
-          </div>
-          <div className="collapse-content">
-            <ul>
-              <li>2021 HUFStudy 대상</li>
-            </ul>
-          </div>
-        </div>
+      <div className="bg-[#F3F3F3] p-6 sm:p-10 sm:px-20 mt-10 rounded-3xl text-xs sm:text-base">
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+            </div>
+            <div className="timeline-end md:text-end mb-10">
+              <time className="font-mono italic">~2021</time>
+              <div>
+                <div>
+                  ICPC 본선 진출
+                </div>
+                <div>
+                  OIDC 오픈 인프라 경진대회 본선 진출
+                </div>
+                <div>
+                  스타트업 코딩 페스티벌 본선 진출
+                </div>
+                <div>
+                  K-hackathon 본선 진출
+                </div>
+                <div>
+                  K-SW스퀘어 프로그램 합격
+                </div>
+                <div>
+                  부스트코스 대학생 서포터즈 2기
+                </div>
+                <div>
+                  웨일 확장앱 개발대회 참가
+                </div>
+                <div>
+                  FIDO 해커톤 참가
+                </div>
+              </div>
+            </div>
+            <hr />
 
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+            </div>
+            <div className="timeline-end mb-10">
+              <time className="font-mono italic">2022</time>
+              <div>
+                <div>
+                  너디너리 제 2회 해커톤 1등 수상
+                </div>
+                <div>
+                  고려대학교 해커톤 2등(쿼드마이너상) 수상
+                </div>
+                <div>
+                  HUFS 오픈소스 협업 Hackalearn 해커톤 ‘가장 완성도 높은 서비스’상 수상
+                </div>
+                <div>
+                  HUFS 하계계 모각코 상위 우수 선정(2팀)
+                </div>
+                <div>
+                  한국외대 제 1회 Summer 해커톤 주최 총괄 담당
+                </div>
+                <div>
+                  HUFS H-UP 최우수상
+                </div>
+                <div>
+                  HUFS 교내 알고리즘 대회 비기너 트랙 입상
+                </div>
+                <div>
+                  HUFS Code Festival 은상 수상
+                </div>
+              </div>
+            </div>
+            <hr />
+          </li>
+          <li>
 
-        <div className="collapse bg-base-200">
-          <input type="checkbox" />
-          <div className="collapse-title text-xl font-medium">
-            <ul className="list-disc font-bold text-xs sm:text-base">
-              <li>2022 너디너리 제 2회 해커톤 1등 수상</li>
-              <li>2022 고려대학교 해커톤 2등(쿼드마이너상) 수상</li>
-              <li>
-                2022 HUFS 오픈소스 협업 Hackalearn 해커톤 ‘가장 완성도 높은 서비스’
-                상 수상
-              </li>
-              <li>2022 HUFS 하계계 모각코 상위 우수 선정(2팀)</li>
-              <li>Flutter Festival Session Speaker 참여</li>
-              <li>한국외대 제 1회 Summer 해커톤 주최 총괄 담당</li>
-              <li>2021 HUFStudy 대상</li>
-              <li>2022 메타버스 개발자 경진대회 3차 본선 합격</li>
-              <li>BlitzLabs X2E Ideathon 결선 진출</li>
-              <li>2022 HUFS H-UP 최우수상</li>
-              <li>튜터링 프로그램 우수팀 선정</li>
-              <li>2022 UMC 앱 런칭 프로젝트 우수상</li>
-              <li>ICPC 본선 진출</li>
-              <li>OIDC 오픈 인프라 경진대회 본선 진출</li>
-              <li>스타트업 코딩 페스티벌 본선 진출</li>
-              <li>GDSC X TAB 연합 해커톤 수상</li>
-              <li>K-hackathon 본선 진출</li>
-            </ul>
-          </div>
-          <div className="collapse-content">
-            <p>
-              <ul className="list-disc font-bold text-xs sm:text-base">
-                <li>HUFS 교내 알고리즘 대회 비기너 트랙 입상</li>
-                <li>HUFS Code Festival 은상 수상</li>
-                <li>K-SW스퀘어 프로그램 합격</li>
-                <li>부스트코스 대학생 서포터즈 2기</li>
-                <li>웨일 확장앱 개발대회 참가</li>
-                <li>FIDO 해커톤 참가</li>
-                <li>2023 벤쳐 스타트업 아카데미 해커톤 대상 수상</li>
-                <li>2023 중소벤처기업부 장관상 수상</li>
-                <li>2023 SPARCS 카이스트 해커톤 참가</li>
-                <li>2023 한국외국어대학교 AI/SW 창업프로젝트 우수팀 선정</li>
-                <li>코드잇 앰버서더 1기</li>
-                <li>2023 벤처스타트업 아카데미 해커톤 대상(1위, 중소벤처기업부장관상) 수상 </li>
-                <li>2023 서울시우먼테크해커톤 우수상(3위, 서울시여성가족재단 대표이사상) 수상 </li>
-                <li>2023 4th UMC Hackathon 우수상 수상</li>
-                <li>2023 SW 중심대학 공동해커톤 최우수상(2위, 정보통신기획평가원장상) 수상 </li>
-                <li>2023 SKT 대학생 테크 인플루언서 데보션영 2기</li>
-                <li>한국외대 제 2회 HUFSummber Hackathon 주최</li>
-              </ul>
-            </p>
-          </div>
-        </div>
+            <div className="timeline-middle">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+            </div>
+            <div className="timeline-end md:text-end mb-10">
+              <time className="font-mono italic">2023</time>
+              <div>
+                <div>
+                  벤쳐 스타트업 아카데미 해커톤 대상 수상
+                </div>
+                <div>
+                  중소벤처기업부 장관상 수상
+                </div>
+                <div>
+                  SPARCS 카이스트 해커톤 참가
+                </div>
+                <div>
+                  코드잇 앰버서더 1기
+                </div>
+                <div>
+                  벤처스타트업 아카데미 해커톤 대상(1위, 중소벤처기업부장관상) 수상
+                </div>
+                <div>
+                  서울시우먼테크해커톤 우수상(3위, 서울시여성가족재단 대표이사상) 수상
+                </div>
+                <div>
+                  4th UMC Hackathon 우수상 수상
+                </div>
+                <div>
+                  SW 중심대학 공동해커톤 최우수상(2위, 정보통신기획평가원장상) 수상
+                </div>
+                <div>
+                  SKT 대학생 테크 인플루언서 데보션영 2기
+                </div>
+                <div>
+                  한국외대 제 2회 HUFSummber Hackathon 주최
+                </div>
+              </div>
+            </div>
+            <hr />
+          </li>
+
+        </ul>
+
 
 
       </div>
